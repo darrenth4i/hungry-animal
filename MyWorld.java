94458268e73be0e72a22f27dff08eeae9d50ca59
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2023-11-21 13:02:30 -0500

    Add random spawning apple at ceiling

--- MyWorld.java
+++ MyWorld.java
@@ -20,5 +20,14 @@ public class MyWorld extends World
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
+        
+        createApple();
+    }
+    
+    public void createApple(){
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
